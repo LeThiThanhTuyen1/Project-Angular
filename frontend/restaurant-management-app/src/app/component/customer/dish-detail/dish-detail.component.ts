f487b5480e5d6fd03650cdd5da4f6ac9dda2eff7
NguyenThiKieuOanh24 <oanhkieu2425@gmail.com>
2024-06-25 10:30:14 +0700

    one

--- frontend/restaurant-management-app/src/app/component/customer/dish-detail/dish-detail.component.ts
+++ frontend/restaurant-management-app/src/app/component/customer/dish-detail/dish-detail.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DishService } from '../../../services/dish.service';
+import { Dish } from '../../../models/dish.model';
 @Component({
   selector: 'app-dish-detail',
   templateUrl: './dish-detail.component.html',
-  styleUrl: './dish-detail.component.css'
+  styleUrls: ['./dish-detail.component.css']
 })
-export class DishDetailComponent {
+export class DishDetailComponent implements OnInit {
+  dish: Dish | null = null;
 
+  constructor(
+    private route: ActivatedRoute,
+    private dishService: DishService
+  ) { }
+
+  ngOnInit(): void {
+    this.getDishDetail();
+  }
+
+  getDishDetail(): void {
+    const id = +this.route.snapshot.paramMap.get;
+    this.dishService.getDishById(id).subscribe(dish => {
+      this.dish = dish;
+    }, error => {
+      console.error('Error fetching dish details:', error);
+    });
+  }
 }
+
